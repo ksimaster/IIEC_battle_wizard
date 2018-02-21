0cdd4525ee17bb99aca64ffa7a24f9621b4f0828
ksimaster <36484548+ksimaster@users.noreply.github.com>
2018-02-21 20:34:43 +0300

    Скрипт движения, добавлен и убран 3d person
    Написан простейший скрипт движения, попробован 3d person, убран из-за кривости

--- IIEC_battle_wizard/Assets/SimpleMove.cs
+++ IIEC_battle_wizard/Assets/SimpleMove.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class SimpleMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject obj;
+    public float range = 5f;
+    public float moveSpeed = 3f, turnSpeed = 40f;
+
 		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+            obj.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            obj.transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            obj.transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            obj.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+
+
+    }
 }
